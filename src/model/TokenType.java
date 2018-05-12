e3f21df7c65eb006c5c49a0eb084ddfe967734da
Troublor <troublor@live.com>
2018-05-12 14:12:56 +0800

    增加了数据类型的token type

--- src/model/TokenType.java
+++ src/model/TokenType.java
@@ -2,7 +2,8 @@ package model;
 
 public enum TokenType {
     INSTRUCTION, REGISTER, CONSTANT, REMAIN_SETTLE,
-    FAKE_INSTRUCTION, LABEL, BORDER, ERROR_TOKEN, COMMENT;
+    FAKE_INSTRUCTION, LABEL, BORDER, ERROR_TOKEN, COMMENT,
+    DATA_TYPE;
 
     @Override
     public String toString() {
