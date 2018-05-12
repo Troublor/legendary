8cb6f04fc6480ee3446a14b5f5fd4d722dfc0884
Troublor <troublor@live.com>
2018-05-12 16:08:28 +0800

    支持打开文件

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,19 +3,20 @@ package controller;
 import com.sun.istack.internal.NotNull;
 import custom.control.CodeEditor;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import model.ApplicationData;
 
-import java.io.File;
+import java.io.*;
 
 public class MainController {
     private Stage primaryStage;
+
+    private ApplicationData applicationData = new ApplicationData();
     //Controls
     @FXML
     private TreeView<String> projectTreeView;
@@ -23,6 +24,8 @@ public class MainController {
     private Label projectNameLabel;
     @FXML
     private CodeEditor codeEditor;
+    @FXML
+    private TabPane editTabPane;
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -34,15 +37,25 @@ public class MainController {
         directoryChooser.setTitle("打开项目");
         File directory = directoryChooser.showDialog(primaryStage);
         if (directory != null) {
-            this.projectTreeView.setRoot(this.iterateFolder(directory));
+            TreeItem<String> root = this.iterateFolder(directory);
+            root.setExpanded(true);
+            this.projectTreeView.setRoot(root);
             this.projectNameLabel.setText(directory.getName());
+            //设置项目信息
+            applicationData.setRootPath(directory.getAbsolutePath());
         }
+
     }
 
+    /**
+     * 遍历显示文件夹
+     * @param path 文件夹
+     * @return TreeView的节点
+     */
     private TreeItem<String> iterateFolder(@NotNull File path) {
         TreeItem<String> rootItem = new TreeItem<>(path.getName());
         if (path.isDirectory()) {
-            rootItem.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("../../img/folder.png"))));
+            rootItem.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("../img/folder.png"))));
             File[] files = path.listFiles();
             if (files != null) {
                 for (File file : files) {
@@ -51,13 +64,13 @@ public class MainController {
                         item = this.iterateFolder(file);
                     } else {
                         item = new TreeItem<>(file.getName());
-                        item.setGraphic(new ImageView(new Image(this.getClass().getResourceAsStream("../../img/file.png"))));
+                        item.setGraphic(new ImageView(new Image(this.getClass().getResourceAsStream("../img/file.png"))));
                     }
                     rootItem.getChildren().add(item);
                 }
             }
         } else {
-            rootItem.setGraphic(new ImageView(new Image(this.getClass().getResourceAsStream("../../img/file.png"))));
+            rootItem.setGraphic(new ImageView(new Image(this.getClass().getResourceAsStream("../img/file.png"))));
         }
         return rootItem;
     }
@@ -65,5 +78,36 @@ public class MainController {
 
     public void stop() {
         codeEditor.stop();
+    }
+
+    public void TreeViewOnDoubleClicked(MouseEvent mouseEvent) {
+        if (mouseEvent.getClickCount() == 2) {
+            TreeItem<String> item = projectTreeView.getSelectionModel().getSelectedItem();
+            String fileName = item.getValue();
+            CodeEditor codeEditor = new CodeEditor();
+
+            File file = new File(applicationData.getRootPath() + File.separator + fileName);
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((temp = reader.readLine()) != null) {
+                    stringBuilder.append("<p>");
+                    stringBuilder.append(temp);
+                    stringBuilder.append("</p>");
+                }
+                codeEditor.setHtmlText(stringBuilder.toString());
+            } catch (Exception e) {
+                this.sendMessageDialog(e.getMessage());
+            }
+            Tab newTab = new Tab(fileName);
+            newTab.setClosable(true);
+            newTab.setContent(codeEditor);
+            editTabPane.getTabs().add(newTab);
+            editTabPane.getSelectionModel().select(newTab);
+        }
+    }
+
+    private void sendMessageDialog(String msg) {
+
     }
 }
---
+++ src/model/ApplicationData.java
@@ -0,0 +1,14 @@
+package model;
+
+public class ApplicationData {
+    String rootPath;
+
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    public String getRootPath() {
+
+        return rootPath;
+    }
+}
